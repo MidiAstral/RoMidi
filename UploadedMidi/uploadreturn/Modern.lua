af129f29644f70a56b1ab170e18a209f91be6f07
MidiAstral <ajpatiten21@gmail.com>
2026-03-21 02:24:06 +0800

    📝 Remove Mitski_-_My_Love_Mine_All_Mine from index

--- UploadedMidi/uploadreturn/Modern.lua
+++ UploadedMidi/uploadreturn/Modern.lua
@@ -4,7 +4,6 @@ return {
     "Beanie",
     "blue - yung kai",
     "Her - JVKE",
-    "Mitski_-_My_Love_Mine_All_Mine",
     "Tek It - Cafune",
     "Tony Ann - Anxiety",
     "Young Girl A - siinamota (slightly more difficult)",
