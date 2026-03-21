1509fd860c5235271554209a7d995ec7a0a00ecb
MidiAstral <ajpatiten21@gmail.com>
2026-03-21 19:17:10 +0800

    📝 Add Mitski - My Love Mine All Mine to index

--- UploadedMidi/uploadreturn/Modern.lua
+++ UploadedMidi/uploadreturn/Modern.lua
@@ -4,6 +4,7 @@ return {
     "Beanie",
     "blue - yung kai",
     "Her - JVKE",
+    "Mitski - My Love Mine All Mine",
     "Tek It - Cafune",
     "Tony Ann - Anxiety",
     "Young Girl A - siinamota (slightly more difficult)",
