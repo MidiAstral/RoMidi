6fea075bac0e87e77d68a301120ed7e1017668e6
MidiAstral <ajpatiten21@gmail.com>
2026-03-23 03:32:17 +0800

    📝 Add my_light_(我的光)_-_yung_kai to index

--- UploadedMidi/uploadreturn/GameOST.lua
+++ UploadedMidi/uploadreturn/GameOST.lua
@@ -4,7 +4,8 @@ return {
     "Finale - Toby Fox",
     "Gaster's Theme - Toby Fox",
     "MEGALOVANIA - Toby Fox",
+    "my_light_(我的光)_-_yung_kai",
+    "sans. - Toby Fox",
     "Snowy - Toby Fox",
     "Undertale - Toby Fox",
-    "sans. - Toby Fox"
 }
