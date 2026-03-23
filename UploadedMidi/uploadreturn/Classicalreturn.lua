c3278ce7fb5bab4ef2f9ba60c297a54a56844612
MidiAstral <ajpatiten21@gmail.com>
2026-03-24 03:49:58 +0800

    📝 Add MIDI File - Bruno Mars - Risk It All (Easy) to index

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -2,6 +2,7 @@ return {
     "AnyConv.com__5875-ed88-4b61-9772-980efa64c95d",
     "Bruno Mars - Risk It All",
     "in the pool  C",
+    "MIDI File - Bruno Mars - Risk It All (Easy)",
     "Pirates of the Caribbean - He's a Pirate (1)",
     "Risk it all",
     "بروحي فتاة..mid",
