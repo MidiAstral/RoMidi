db345eaee12c4899fe1302b8dd66b525da447214
MidiAstral <ajpatiten21@gmail.com>
2026-03-26 21:20:38 +0800

    📝 Add Pirates of the Caribbean - He's a Pirate (3) to index

--- UploadedMidi/uploadreturn/Movie.lua
+++ UploadedMidi/uploadreturn/Movie.lua
@@ -3,6 +3,7 @@ return {
     "Bohemian Rhapsody - Queen",
     "hans-zimmer-cornfield-chase-interstellar-soundtrack-21091-nonstop2k.com",
     "My heartPIANO",
+    "Pirates of the Caribbean - He's a Pirate (3)",
     "Squid Game musique",
     "Squid Game slow",
     "The Simpsons Main Theme  Danny Elfman MLX",
