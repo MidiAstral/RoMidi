b02dc4663d9ce68b888287af7992de37ca638234
MidiAstral <ajpatiten21@gmail.com>
2026-04-03 12:26:33 +0800

    Update Classicalreturn.lua after batch upload

---
+++ GuitarPresets/returnfiles/Classicalreturn.lua
@@ -0,0 +1,9 @@
+return {
+	"Canon in D",
+	"Fly Me to the Moon for Solo Guitar",
+	"Mozart - Rondo Alla Turca",
+	"My way - Frank Sinatra",
+	"River Flows In You - Yiruma (Guitar)",
+	"The Entertainer for solo guitar",
+	"The Sound Of Silence - Simon & Garfunkel"
+}
