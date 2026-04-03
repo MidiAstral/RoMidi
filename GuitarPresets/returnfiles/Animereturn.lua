f41751a194bf263312bf430f5d7c82dc194c5987
MidiAstral <ajpatiten21@gmail.com>
2026-04-03 12:25:55 +0800

    Update Animereturn.lua after batch upload

---
+++ GuitarPresets/returnfiles/Animereturn.lua
@@ -0,0 +1,5 @@
+return {
+	"Gravity Falls for guitar",
+	"The Merry-Go-Around of Life (Howl's Moving Castle) Fingerstyle Guitar",
+	"You've got a friend in me Main Theme from Toy Story. Randy Newman. Arrangement for Solo Acoustic"
+}
