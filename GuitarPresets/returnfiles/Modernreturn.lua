c30ae4e71938e324434691bb7b424b757764fe0f
MidiAstral <ajpatiten21@gmail.com>
2026-04-03 12:10:10 +0800

    Update Modernreturn.lua after batch upload

--- GuitarPresets/returnfiles/Modernreturn.lua
+++ GuitarPresets/returnfiles/Modernreturn.lua
@@ -1,1 +1,9 @@
-return {}
+return {
+	"Blue-Yung Kai",
+	"Di na muli",
+	"Di na muli guitar",
+	"DINA MULI - THE ITCHYWORMS (GUITAR SOLO PART)",
+	"Ed Sheeran - Shape of You (Guitar cover)",
+	"The winner takes it all - ABBA",
+	"You've got a friend in me Main Theme from Toy Story. Randy Newman. Arrangement for Solo Acoustic"
+}
