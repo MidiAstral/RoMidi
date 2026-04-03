92d02abef22352a558e1912563bd15491f6ff1fd
MidiAstral <ajpatiten21@gmail.com>
2026-04-03 12:20:54 +0800

    Update Modernreturn.lua after batch upload

--- GuitarPresets/returnfiles/Modernreturn.lua
+++ GuitarPresets/returnfiles/Modernreturn.lua
@@ -1,1 +1,9 @@
-return {}
+return {
+	"Blue-Yung Kai",
+	"Di na muli",
+	"Di na muli guitar",
+	"DINA MULI - THE ITCHYWORMS (GUITAR SOLO PART)",
+	"Ed Sheeran - Shape of You (Guitar cover)",
+	"The Sound Of Silence - Simon & Garfunkel",
+	"The winner takes it all - ABBA",
+}
