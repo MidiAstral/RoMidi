4d5046d22cae9516d1f32ef818f0bf154d7b1a84
MidiAstral <ajpatiten21@gmail.com>
2026-04-08 10:21:32 +0800

    📝 Add Stranglers — Golden Brown to index

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -22,6 +22,7 @@ return {
     "Øneheart & reidenshi snowfall piano midi",
     "PULSE  Tony Ann aka my neighbours car alarm 1770203303452",
     "Sinking Town   沈める街 - Yoeko Kurahashi",
+    "Stranglers — Golden Brown",
     "Tally Hall- Hidden in the Sand ",
     "Tony Ann - ICARUS",
     "Tony Ann - Reminisce 2",
