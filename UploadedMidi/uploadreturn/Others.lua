36c0a381b41d32395904a0879becb01167eea075
MidiAstral <ajpatiten21@gmail.com>
2026-04-19 13:26:13 +0800

    📝 Add Rush Sigma Boy not impossible but hard to index

--- UploadedMidi/uploadreturn/Others.lua
+++ UploadedMidi/uploadreturn/Others.lua
@@ -86,6 +86,7 @@ return {
     "Rush Mountain King Optimized not impossible but hard",
     "Rush of Life - Tony Ann",
     "Rush Rush not impossible but hard",
+    "Rush Sigma Boy not impossible but hard",
     "Rush USSR Optimized not impossible but hard",
     "Rush Z not impossible but still hard",
     "sinkingtown",
