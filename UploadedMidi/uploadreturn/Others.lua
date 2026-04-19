7a67b49c278aa902318dd2c3c64f09997423359b
MidiAstral <ajpatiten21@gmail.com>
2026-04-19 18:09:18 +0800

    📝 Add Rush e hard FIXED FIXED FIXED FIXED FIXED to index

--- UploadedMidi/uploadreturn/Others.lua
+++ UploadedMidi/uploadreturn/Others.lua
@@ -80,6 +80,7 @@ return {
     "Rush E 4 Black Midi By S3G",
     "Rush E boss? mhxa",
     "RUSH E but something is OFF!",
+    "Rush e hard FIXED FIXED FIXED FIXED FIXED",
     "Rush E not impossible but still very hard fixed",
     "Rush E Optimized Fixed Fixed Fixed Fixed",
     "Rush E Optimized not impossible but still hard",
