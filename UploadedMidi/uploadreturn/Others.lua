c0e3b9b335c345aa1175b12a44ee789b97a60fdf
MidiAstral <ajpatiten21@gmail.com>
2026-04-23 08:03:44 +0800

    📝 Add Beethoven - Moonlight sonata 3rd Movement IN mhxa to index

--- UploadedMidi/uploadreturn/Others.lua
+++ UploadedMidi/uploadreturn/Others.lua
@@ -10,6 +10,7 @@ return {
     "Alexander Rybak - Fairytale mhxa",
     "At the speed of light(Bloodbath)",
     "AUD NK0247",
+    "Beethoven - Moonlight sonata 3rd Movement IN mhxa",
     "Beethoven - Moonlight sonata 3th movement mhxa",
     "Beta0404",
     "Boywithluke - Problematic mhxa",
