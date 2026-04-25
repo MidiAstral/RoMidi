4ade0c847493ca42be94a3043c7be8832c9dc416
MidiAstral <ajpatiten21@gmail.com>
2026-04-26 05:57:24 +0800

    📋 Update Metal index: +ACDC - Back in Black (3) [MIDIfind.com]

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -1,6 +1,7 @@
 return {
     "A.D.I.D.A.S.",
     "AC DC - Back in Black.mid",
+    "ACDC - Back in Black (3) [MIDIfind.com]",
     "Avenged Sevenfold - Bat Country",
     "BeQuickOrBeDead",
     "Bitchslap",
