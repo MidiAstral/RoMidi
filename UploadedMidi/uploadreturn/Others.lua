6e68fcf349c3af1040a5098062677e273c390a94
MidiAstral <ajpatiten21@gmail.com>
2026-04-25 15:15:36 +0800

    📝 Add Rush SM64 to index

--- UploadedMidi/uploadreturn/Others.lua
+++ UploadedMidi/uploadreturn/Others.lua
@@ -135,6 +135,7 @@ return {
     "Rush Round And Round not impossible",
     "Rush Rush not impossible but hard",
     "Rush Sigma Boy not impossible but hard",
+    "Rush SM64",
     "RUSH SONICSPIANOONIRL REMASTERED",
     "RUSH STEREO MADNESS (Remake)",
     "Rush USSR Optimized not impossible but hard",
