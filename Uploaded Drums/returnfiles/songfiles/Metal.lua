d69464c26a2d21023fa114f060643a68aa8c559e
MidiAstral <ajpatiten21@gmail.com>
2026-04-26 20:38:43 +0800

    📋 Update Metal index: +Avenged Sevenfold - Chapter Four [MIDIfind.com]

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -6,6 +6,7 @@ return {
     "Avenged Sevenfold - Almost Easy",
     "Avenged Sevenfold - Bat Country",
     "Avenged Sevenfold - Beast And The Harlot v7",
+    "Avenged Sevenfold - Chapter Four [MIDIfind.com]",
     "Avenged Sevenfold - Dear God.mid",
     "Avenged Sevenfold - I Won't See You Tonight Part (1) [MIDIfind.com]",
     "Avenged Sevenfold - M.I.A (WIP)",
