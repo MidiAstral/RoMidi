f9cb06e99b30b4c9ebb01868be29a061f4ff6074
MidiAstral <ajpatiten21@gmail.com>
2026-04-29 09:42:46 +0800

    📝 Add GOZALO - Ariis to index

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -14,6 +14,7 @@ return {
     "Flamewall",
     "Fur Elise (1)",
     "Gloria Gaynor Arr.  Elaine Kelkel - I Will Survive",
+    "GOZALO - Ariis",
     "Handel,Halvorsen Passacaglia",
     "in the pool  C",
     "JOPLIN.The entertainer",
