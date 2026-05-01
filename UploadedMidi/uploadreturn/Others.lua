afbd50970b979878409aba7e90967c51b65a8232
MidiAstral <ajpatiten21@gmail.com>
2026-05-01 21:58:02 +0800

    📝 Add tegrtstrgy to index

--- UploadedMidi/uploadreturn/Others.lua
+++ UploadedMidi/uploadreturn/Others.lua
@@ -183,6 +183,7 @@ return {
     "Tally Hall - The Bidding",
     "Tally+Hall+-+Turn+The+Lights+Off",
     "tau the song playable",
+    "tegrtstrgy",
     "tháng 4là lời nói dối của em",
     "Thang dien impossible lol",
     "the entertainer",
