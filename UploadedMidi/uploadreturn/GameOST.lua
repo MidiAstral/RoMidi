c0ccc60b60bcf157e9097154fb256fa1a1ce036b
MidiAstral <ajpatiten21@gmail.com>
2026-05-02 15:34:16 +0800

    📝 Add After School Jamboree to index

--- UploadedMidi/uploadreturn/GameOST.lua
+++ UploadedMidi/uploadreturn/GameOST.lua
@@ -1,6 +1,7 @@
 return {
     " Until-Then-OST--Memories",
     "[Black MIDI] Bad Apple!! 1 Million Notes v3 (fixed piano)",
+    "After School Jamboree",
     "Animation Warrior Theme - Nighthawk22",
     "ASGORE - Toby Fox",
     "Attack Of The Killer Queen - (Deltarune) - [MIDI]",
